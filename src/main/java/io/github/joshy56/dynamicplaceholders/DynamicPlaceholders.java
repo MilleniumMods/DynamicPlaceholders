a796e92cc853ccaeb51786ef5826ee5b9ac1afac
joshy23 <agustinelmejor38@gmail.com>
2022-06-13 21:47:50 -0300

    Changes

--- src/main/java/io/github/joshy56/dynamicplaceholders/DynamicPlaceholders.java
+++ src/main/java/io/github/joshy56/dynamicplaceholders/DynamicPlaceholders.java
@@ -1,9 +1,30 @@
 package io.github.joshy56.dynamicplaceholders;
 
+import io.github.joshy56.dynamicplaceholders.hook.PluginPlaceholderExpansion;
+import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 /**
  * Created by joshy23 (justJoshy23 - joshy56) on 10/6/2022.
  */
 public class DynamicPlaceholders extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        PluginPlaceholderExpansion expansion = new PluginPlaceholderExpansion(this, "principalKey");
+        expansion.register();
+        expansion.getPlaceholdersProcessors().put(
+                "shoots",
+                optionalPlayer -> optionalPlayer
+                        .filter(Objects::nonNull)
+                        .map(
+                                player -> {
+                                    player.sendMessage("Se chequearon tus disparos");
+                                    return String.valueOf((Math.random() * 2) + 1);
+                                }
+                        ).orElse("None")
+        );
+    }
 }
--- src/main/java/io/github/joshy56/dynamicplaceholders/hook/PluginPlaceholderExpansion.java
+++ src/main/java/io/github/joshy56/dynamicplaceholders/hook/PluginPlaceholderExpansion.java
@@ -3,19 +3,26 @@ package io.github.joshy56.dynamicplaceholders.hook;
 import com.google.common.base.Preconditions;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
 
 /**
  * Created by joshy23 (justJoshy23 - joshy56) on 10/6/2022.
  */
 public class PluginPlaceholderExpansion extends PlaceholderExpansion {
     private final NamespacedKey identifier;
-    private final int version;
-    private final String author;
+    private final String version, author;
+    private final Map<String, Function<Optional<Player>, String>> placeholdersProcessors;
 
-    public PluginPlaceholderExpansion(@NotNull Plugin namespace, @NotNull String key, @NotNull String value){
+    public PluginPlaceholderExpansion(@NotNull Plugin namespace, @NotNull String key){
         identifier = Preconditions.checkNotNull(
                 NamespacedKey.fromString(
                         key,
@@ -23,26 +30,45 @@ public class PluginPlaceholderExpansion extends PlaceholderExpansion {
                 ),
                 "Pto"
         );
-        
+        author = Optional.of(namespace.getDescription().getAuthors())
+                .filter(authors -> !authors.isEmpty())
+                .map(authors -> authors.get(0))
+                .orElse(namespace.getName());
+        version = namespace.getDescription().getVersion();
+        placeholdersProcessors = new HashMap<>();
     }
 
     @Override
     public @NotNull String getIdentifier() {
-        return null;
+        return identifier.asString();
     }
 
     @Override
     public @NotNull String getAuthor() {
-        return null;
+        return author;
     }
 
     @Override
     public @NotNull String getVersion() {
-        return null;
+        return version;
     }
 
     @Override
     public @Nullable String getRequiredPlugin() {
-        return super.getRequiredPlugin();
+        return getIdentifier();
+    }
+
+    @Override
+    public @NotNull List<String> getPlaceholders() {
+        return placeholdersProcessors.keySet().stream().toList();
+    }
+
+    public Map<String, Function<Optional<Player>, String>> getPlaceholdersProcessors() {
+        return placeholdersProcessors;
+    }
+
+    @Override
+    public @Nullable String onPlaceholderRequest(@Nullable Player player, @NotNull String params) {
+        return placeholdersProcessors.containsKey(params) ? placeholdersProcessors.get(params).apply(Optional.ofNullable(player)) : null;
     }
 }
